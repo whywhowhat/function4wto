709c5411078e2e7c656eab1a691a9177e491e717
whywhowhat <jin.soo.park.1991@gmail.com>
2022-05-12 23:34:39 +0200

    Update FIMO.r

--- FIMO.r
+++ FIMO.r
@@ -1,3 +1,36 @@
+
+# installing package seperately 
+install.packages("dplyr")
+install.packages("magrittr")
+install.packages("stringr")
+BiocManager::install("GenomicFeatures",force = TRUE  )
+BiocManager::install("GenomicRanges",force = TRUE  )
+BiocManager::install("TxDb.Hsapiens.UCSC.hg38.knownGene",force = TRUE ) 
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg38",force = TRUE )
+BiocManager::install("MotifDb",force = TRUE)
+BiocManager::install("universalmotif",force = TRUE)
+BiocManager::install("biomaRt",force = TRUE)
+BiocManager::install("plyranges",force = TRUE)
+BiocManager::install("Repitools")#
+
+
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(magrittr))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(GenomicFeatures))
+suppressPackageStartupMessages(library(GenomicRanges))
+suppressPackageStartupMessages(library(TxDb.Hsapiens.UCSC.hg38.knownGene))
+suppressPackageStartupMessages(library(BSgenome.Hsapiens.UCSC.hg38))
+suppressPackageStartupMessages(library(MotifDb))
+suppressPackageStartupMessages(library(universalmotif))
+suppressPackageStartupMessages(library(biomaRt))
+suppressPackageStartupMessages(library(plyranges))
+suppressPackageStartupMessages(library(Repitools))
+suppressPackageStartupMessages(library(memes))
+# should localy installed
+library(memes)
+
+
 FIMO <- function(promter_seq , TFlist, thres  ){
 if(is.null(promter_seq) == F){
     stop("promter_seq should not be empty.")
